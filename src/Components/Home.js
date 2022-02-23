5cb90ec1e7c68953370ad81e752225c7d0c49e5a
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-02-24 00:35:45 +0100

    added functionality to the modal and sidebar buttons

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {FaBars} from 'react-icons/fa'
+import {useGlobalContext} from '../context'
 
 const Home = () => {
+    const {openSidebar, openModal} = useGlobalContext()
     return (
         <main>
-            <button className='sidebar-toggle'><FaBars /></button>
-            <button className='btn'>Show modal</button>
+            <button onClick={openSidebar} className='sidebar-toggle'><FaBars /></button>
+            <button className='btn' onClick={openModal}>Show modal</button>
         </main>
     )
 
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,12 +2,14 @@ import React from "react";
 import {FaTimes} from 'react-icons/fa'
 import { links, social } from "../Data";
 import logo from '../logo.svg'
+import { useGlobalContext } from "../context";
 
 const Sidebar =() => {
-    return <aside className="sidebar show-sidebar">
+    const {isSidebarOpen, closeSidebar} = useGlobalContext()
+    return <aside className={`${isSidebarOpen?'sidebar show-sidebar':'sidebar'}`}>
         <div className="sidebar-header">
             <img src={logo} className = "logo" alt = 'logo'/>
-            <button className="close-btn"> <FaTimes /></button>
+            <button className="close-btn" onClick={closeSidebar}> <FaTimes /></button>
         </div>
         <ul className="links">
             {links.map((link)=>{
